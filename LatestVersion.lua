d59781f87263d2d0c31d39e87a00b346a79419d5
Sap <162757246+Scaroontop@users.noreply.github.com>
2025-02-17 11:55:58 -0500

    Update LatestVersion.lua

--- LatestVersion.lua
+++ LatestVersion.lua
@@ -1,1 +1,10 @@
-0.0.0
+    local latestVersion = "0.0.1"  
+
+    if version ~= latestVersion then
+        print("A new update is available!")
+    else
+        print("You are using the latest version.")
+    end
+end
+
+checkForUpdates()
